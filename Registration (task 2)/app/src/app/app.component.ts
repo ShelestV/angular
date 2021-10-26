7c80d149f5e212bc90988e08fbf59a926ea008ce
vladimirsh <starvova2002@gmail.com>
2021-10-26 14:08:00 +0300

    finish, but it does not work validation for birthdate on nonreactive form

--- Registration (task 2)/app/src/app/app.component.ts
+++ Registration (task 2)/app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  maxDate;
+
   isActiveForm=false;
+  isActiveReactiveForm=false;
+  isSuccessfulRegistration=false;
+
+  repeatPassword='';
+  password='';
+
   user={
     firstName:'',
     secondName:'',
@@ -17,11 +26,53 @@ export class AppComponent {
     phoneNumber:'',
     birthDate:'',
     password:'',
-    repeatPassword:'',
+  }
+
+  reactiveUser = new FormGroup({
+    "firstName": new FormControl("", [
+      Validators.required,
+    ]),
+    "secondName": new FormControl("", [
+      Validators.required,
+    ]),
+    "login": new FormControl("", [
+      Validators.required,
+      Validators.pattern("[a-zA-Z0-9]*"),
+    ]),
+    "email": new FormControl("", [
+      Validators.required,
+      Validators.email,
+    ]),
+    "phoneNumber": new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[0-9]{10}$"),
+    ]),
+    "birthDate": new FormControl("", [
+      Validators.required,
+      this.isCorrectDate(),
+    ]),
+    "password": new FormControl("", [
+      Validators.required,
+      Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$"),
+    ]),
+    "repeatPassword": new FormControl("", [
+      Validators.required,
+      this.confirmPasswords(),
+    ])
+  });
+
+  constructor() {
+    let now=new Date();
+
+    let maxYear=now.getFullYear()-16;
+    let maxMonth=now.getMonth();
+    let maxDay=now.getDay();
+
+    this.maxDate=new Date(maxYear, maxMonth, maxDay).toISOString();
   }
 
   openReactiveForm() {
-
+    this.isActiveReactiveForm = true;
   }
 
   openForm() {
@@ -29,6 +80,37 @@ export class AppComponent {
   }
 
   signUp() {
+    if (this.isActiveForm) {
+      console.log(JSON.stringify(this.user));
+    }
+    if (this.isActiveReactiveForm) {
+      console.log(JSON.stringify({
+        firstName: this.reactiveUser.get('firstName')?.value,
+        secondName: this.reactiveUser.get('secondName')?.value,
+        login: this.reactiveUser.get('login')?.value,
+        email: this.reactiveUser.get('email')?.value,
+        phoneNumber: this.reactiveUser.get('phoneNumber')?.value,
+        birthDate: this.reactiveUser.get('birthDate')?.value,
+        password: this.reactiveUser.get('password')?.value,
+      }));
+    }
 
+    this.isSuccessfulRegistration = true;
+    this.isActiveForm = false;
+    this.isActiveReactiveForm = false;
+  }
+
+  private isCorrectDate():ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean} | null => {
+      let valid = control.value < this.maxDate;
+      return valid ? null : { date: true };
+    };
+  }
+
+  private confirmPasswords():ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean} | null => {
+      let valid = this.password === control.value;
+      return valid ? null : { confirm: true };
+    };
   }
 }
